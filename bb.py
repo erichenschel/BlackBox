66fefe03bcc58f6210f3dac0376863e29a69a6bb
Eric <eric.henschel7@gmail.com>
2020-04-18 13:56:00 -0400

    moving

--- bb.py
+++ bb.py
@@ -147,7 +147,7 @@ class Game:
 
     # list of nodes along the path of ray
     def rayPath(self, start, board):
-        path = [start]
+        path = []
         currNode = None
         currPos = 0
         exit = False
@@ -173,33 +173,73 @@ class Game:
             return row, col-1
         
         while not exit:
-            # rays current position on board
-            row = path[currPos][0]
-            col = path[currPos][1]
-            print(row, col)
+            
+            # moving down
+
+            # rays current position on board is start
+            if currPos == 0:
+                row = start[0]
+                col = start[1]
+
+                # starting space is empty
+                if board[row][col] == 0:
+                    path.append(start)
+                    currPos +=1
+
+                # starting space is a force field right above node (hit)
+                elif board[row][col] == 1 and board[row+1][col] == 2:
+                    path.append(start)
+                    # break loop
+                    exit = True
+
+                # starting space is occupied by a node (hit)
+                else:
+                    # break loop
+                    exit = True
+            # currPos != start (path array is not empty)
+            else:
+                row = path[currPos][0]
+                col = path[currPos][1]
+                print(row, col)
+
 
             # moving down -- change row (increase)
-            if row == 0:
-                # hit
-                if board[row][col] == 2 or board[row+1][col] == 2:
-                    return 0
-                
-                # reflection - to the right
-                if board[row][col] == 1 and board[row][col-1] == 2:
-                    return 0
-                
-                # reflection - to the left
-                if board[row][col] == 1 and board[row][col+1] == 2:
-                    return 0
-
-                # deflection - right
-                if board[row][col] == 1 and board[row+1][col-1] == 2:
-                    return 0
-                
-                # deflection - left
-                if board[row][col] == 1 and board[row+1][col+1] == 2:
-                    return 0
+            # direct hit (node on edge of board)
+            if board[row][col] == 2:
+                return path
+            # able to occupy the first space but node is in next space
+            elif board[row+1][col] == 2:
+                row = (row, col)
+                currPos = (row, col)
+                path.append(currPos)
+                return path
             
+            # reflection - to the right
+            if board[row][col] == 1 and board[row][col-1] == 2:
+                currPos = (row, col)
+                path.append(currPos)
+                return path
+            
+            # reflection - to the left
+            if board[row][col] == 1 and board[row][col+1] == 2:
+                col += 1
+                currPos = (row, col)
+                path.append(currPos) 
+
+            # deflection - right
+            if board[row][col] == 1 and board[row+1][col-1] == 2: 
+                col += 1
+                currPos = (row, col)
+                path.append(currPos) 
+            # deflection - left
+            if board[row][col] == 1 and board[row+1][col+1] == 2:
+                col -= 1
+                currPos = (row, col)
+                path.append(currPos)
+           
+
+
+           # -------------------------------------------------------
             # moving up -- change row (decrease)
             if row == self.boardSize-1:
                 # hit
