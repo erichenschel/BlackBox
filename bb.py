5ada10294d163ca1e5153829a35399211bece826
Eric <eric.henschel7@gmail.com>
2020-04-17 21:56:34 -0400

    rayLocation

--- bb.py
+++ bb.py
@@ -101,30 +101,41 @@ class Game:
     def rayLocation(self):
         col = None
         row = None
-        c1 = input("Select a firing position (top, bottom, right, left): ")
-        # top - row == 0
-        if c1 == 'top':
-            row = 0
-        # bottom - row == 7
-        elif c1 == 'bottom':
-            row = 7
-        # right - col == 7
-        elif c1 == 'right':
-            col = 7
-        # left - col == 0
-        elif c1 == 'left':
-            col = 0
 
-        while True:
-            pos = int(input("Choose your position (0 - 7): "))
-            if pos > self.boardSize-1:
+        c1 = None
+        pos = None
+
+        while c1 == None:
+            c1 = input("Select a firing position (top, bottom, right, left): ")
+            # top - row == 0
+            if c1 == 'top':
+                row = 0
+            # bottom - row == 7
+            elif c1 == 'bottom':
+                row = 7
+            # right - col == 7
+            elif c1 == 'right':
+                col = 7
+            # left - col == 0
+            elif c1 == 'left':
+                col = 0
+            else:
+                c1 = None
                 print("Error: Invalid entry.")
-                print("Please select an integer between 0 and ", self.boardSize-1)
+
+        while pos == None:
+            pos = input("Choose your position (0 - 7): ")
+            if pos == int(pos):
+                pos = int(pos)
             
-            if pos < self.boardSize and col != None:
+            if pos > self.boardSize-1:
+                print("Error: Invalid entry.") 
+            elif pos < self.boardSize and col != None:
                 return pos, col
             elif pos < self.boardSize and row != None:
                 return row, pos
+            else:
+                pos = None
 
     def rayExit(self):
 
